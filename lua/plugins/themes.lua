f68bb4a7e814eac142da59a28eed548b1b77e03a
Dante Vazquez <74936894+dantevazquez@users.noreply.github.com>
2026-03-02 13:41:17 -0600

    new colorscheme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,11 +1,16 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "warm",
+    },
+  },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "onedark",
     },
   },
 }
